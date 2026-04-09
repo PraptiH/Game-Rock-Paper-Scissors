736867aa4b14314d4e4f0e358861875a2686f535
PraptiH <praptiantara1892@gmail.com>
2026-04-09 22:04:58 +0600

    Generate function to play the game and show text for winner

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 let userScore = 0;
 let computerScore = 0;
+let userWin=true
+let message = document.getElementById("message")
 let selects = document.querySelectorAll('.select')
 selects.forEach(select=>{
     select.addEventListener('click',()=>{
         console.log('click')
         const userChoice = select.getAttribute("id")
+        playGame(userChoice)
     })
 })
-const computerChoice =()=>{
+const generateComputerChoice =()=>{
     const options = ['rock','paper','scissor']
     const randomOption =Math.floor(Math.random()*3)
     return options[randomOption]
 
 }
+const playGame =(userChoice)=>{
+ console.log("user",userChoice)
+ const computerChoice = generateComputerChoice()
+ console.log("comupter",computerChoice)
+ if(userChoice===computerChoice){
+    message.innerText=`It's a draw. Play again`
+ }
+ else if(userChoice==='rock'){
+       userWin = computerChoice==="paper"?false:true
+       message.innerText=`Your ${userChoice} win against ${computerChoice}.`
+ }
+ else if(userChoice==='paper'){
+       userWin = computerChoice==="scissor"?false:true
+       message.innerText=`Your ${userChoice} win against ${computerChoice}.`
+ }
+ else if(userChoice==='scissor'){
+       userWin = computerChoice==="rock"?false:true
+       message.innerText=`Your ${userChoice} win against ${computerChoice}.`
+ }
+}
+
