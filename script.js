5aef8995e72093d092bb4afa0d4c8e5a9033dd98
PraptiH <praptiantara1892@gmail.com>
2026-04-09 20:27:13 +0600

    generate function for computer choice

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+let userScore = 0;
+let computerScore = 0;
 let selects = document.querySelectorAll('.select')
 selects.forEach(select=>{
     select.addEventListener('click',()=>{
         console.log('click')
-        console.log(select.getAttribute("id"))
+        const userChoice = select.getAttribute("id")
     })
 })
+const computerChoice =()=>{
+    const options = ['rock','paper','scissor']
+    const randomOption =Math.floor(Math.random()*3)
+    return options[randomOption]
+
+}
